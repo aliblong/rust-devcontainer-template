183fa20c4ab64bca54782a3ce436135f47a13e9e
Aaron Liblong <liblonga@hotmail.com>
2025-12-14 23:21:08 +0000

    docs: clarify motivation for using devcontainer git scripts

--- .devcontainer/dump-host-gitconfig.sh
+++ .devcontainer/dump-host-gitconfig.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 
-# Dumps host git config in workspace dir, where the devcontainer can access it
+# Dumps host git config in workspace dir, where the devcontainer can read it in.
+# This gets around limitations with the use of the includeIf directive in the config.
 TMP_DIR=tmp-gitconfig
 mkdir -p "$TMP_DIR"
 git config -l --global --include > "$TMP_DIR/.gitconfig.global"
--- .devcontainer/hydrate-devcont-gitconfig.sh
+++ .devcontainer/hydrate-devcont-gitconfig.sh
@@ -4,6 +4,9 @@ set -eu
 # Hydrates devcontainer global git config with dumped host git config.
 # Written by @stewartadam on GitHub
 # https://github.com/microsoft/vscode-remote-release/issues/2084#issuecomment-2289987894
+# The reason to use this rather than simply mounting host config files is that my personal
+# global git config uses the includeIf directive in the usual way (referring to a parent
+# directory on the host), which doesn't port well to devcontainers.
 TMP_DIR=tmp-gitconfig
 for conf in .gitconfig.global .gitconfig.local; do
   if [ -f "$TMP_DIR/$conf" ]; then
